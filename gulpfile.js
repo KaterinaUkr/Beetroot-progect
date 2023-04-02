b5bd33d049568b22dc67a53e4e30b3e0067707b3
KaterinaUkr <sharavarova@gmail.com>
2023-04-02 09:17:58 +0300

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,23 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 const notify = require("gulp-notify");
+const autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('autoprefixer', () => {
+  gulp.src('./*.css')
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(gulp.dest('./'))
+  
+    done();
+});
 
 gulp.task('sass', () => {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass())
+    .pipe(autoprefixer())
     .on('error', (err) => {
       notify().write(err);
       this.emit('end');
