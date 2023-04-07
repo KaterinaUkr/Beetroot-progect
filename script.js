3a24fab6eb7f1e43ad3e20dcdd8932a15f3afc44
KaterinaUkr <sharavarova@gmail.com>
2023-04-07 11:47:56 +0300

    add changes

---
+++ script.js
@@ -0,0 +1,21 @@
+/*let firstName = 'Ivan';
+let age = 27;*/
+
+//let result = firstName + ' has ' + age + ' years!'
+//let result = `${firstName} has ${age} years`
+
+/*const firstNumber = prompt('How much mile in km');
+const second_Number = 0.621371192;
+
+const result = Number(firstNumber) * Number(second_Number) + 'mile' ;
+
+alert(result)*/
+
+//console.log(result);
+
+const stavka = 250;
+const percent = 0.1;
+const prodag = prompt('How much are you sale?');
+
+const result = stavka + (percent * Number(prodag));
+alert(`your salary - ${result} $`)
