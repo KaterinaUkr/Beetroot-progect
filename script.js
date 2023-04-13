a931833f294ca7b55d76d795306a3af9d9e154ce
KaterinaUkr <sharavarova@gmail.com>
2023-04-13 14:19:32 +0300

    add changes

---
+++ script.js
@@ -0,0 +1,121 @@
+    // Minimum
+
+// const age = prompt('How old are you?');
+// if (age >= 0 && age <= 11) {
+//     alert("You are a child");
+// } else if (age >= 12 && age <= 17) {
+//     alert("You are a teenager");
+// } else if (age >= 18 && age <= 59) {
+//     alert("You are a adult");
+// } else {
+//     alert("You are a pensioner");
+// };
+
+// const number = +prompt('Enter any number from 0 to 9');
+// switch (number) {
+//     case 0:
+//         alert(')');
+//         break;
+//     case 1:
+//         alert('!');
+//         break;
+//     case 2:
+//         alert('@');
+//         break;
+//     case 3:
+//         alert('#');
+//         break;
+//     case 4:
+//         alert('$');
+//         break;
+//     case 5:
+//         alert('%');
+//         break;
+//     case 6:
+//         alert('^');
+//         break;
+//     case 7:
+//         alert('&');
+//         break;
+//     case 8:
+//         alert('*');
+//         break;
+//     case 9:
+//         alert('(');
+//         break;
+//     default:
+//         break;
+// }
+
+// let firstNumber = +prompt('Enter first number');
+// const secondNumber = +prompt('Enter second number');
+// let sum = 0;
+
+// while (firstNumber <= secondNumber) {
+//     if (firstNumber <= secondNumber) {
+//         sum += firstNumber;
+//     }
+//     firstNumber++;
+// }
+// alert(sum);
+
+
+// let x = +prompt('Enter first number');
+// let y = +prompt('Enter second number');
+
+// while (x != 0 && y != 0) {
+    
+//     if (x > y) {
+//         x = x % y;
+        
+//     } else {
+//         y = y % x;
+//     }
+// }
+// alert(`Greatest common divisor ${x+y}`);
+
+
+// let number = +prompt('Enter any number');
+// for (let i = 1; i <= number; i++) {
+//     if (number % i == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+    // Norma 
+    
+// let number = prompt('Enter a five-digit number');
+//     if (number[0] === number[4] && number[1] === number[3]) {
+//         alert('Your number is a palindrom');
+//     } else {
+//         alert('Your number is not a palindrom');
+//     }
+
+// let shoppingSum = +(prompt('Яка сума покупки ?'));
+// let result;
+    
+// if (shoppingSum >= 200 && shoppingSum < 300) {
+//     result = shoppingSum - (shoppingSum * 0.03);
+// } else if (shoppingSum >= 300 && shoppingSum < 500) {
+//     result = shoppingSum - (shoppingSum * 0.05);
+// } else if (shoppingSum >= 500) {
+//     result = shoppingSum - (shoppingSum * 0.07);
+// } else {
+//     result = "Wrong data!";
+// };
+// alert(result);
+
+
+let arrayNumber = [ ];
+  //задаєм длину масиву
+for (arrayNumber.length = 0; arrayNumber.length <= 9; arrayNumber.length++) {
+    arrayNumber.push = arrayNumber += prompt('Enter any number?');  
+    console.log(arrayNumber);
+}
+  alert(arrayNumber);
+
+    
+
+    // Maximum
