153467818a5b27599ec109a07e45864934f37860
KaterinaUkr <sharavarova@gmail.com>
2023-04-15 10:01:55 +0300

    add homework

--- script.js
+++ script.js
@@ -1,23 +1,76 @@
 // Скасування функції setTimeout відбувається за допомогою clearTimeout(назва функції)
 
+    /* 1. Варіанти створення функцій -
+    - оголошення функції
+        function name(parametr1, parametr 2,...) {
+         alert('Hello!')
+        }
+        name();
 
-    //Factorial
+    - функціональний вираз
+        let name = function (parametr1, parametr 2,...) {
+         alert('Hello!')
+        }
+        name();*/
 
-function factorial(n) {
-    let result = 1;   
-    for (let i = 1; i <= n; i++) {
-        result *= i;       
+
+    // 2.
+// const calcArg = function () {
+    
+//     console.log(arguments);
+//     return arguments;
+// }
+// console.log(calcArg(25, 'once', 36, 'ghnjmcg'));
+
+
+    // 3.
+// function number(num1, num2) {
+//     if (num1 < num2) {
+//         return `-1`;
+//     } else if (num1 > num2) {
+//         return `1`;
+//     } else {
+//         return `0`;
+//     }
+// }
+
+
+    //4. Factorial
+
+// function factorial(n) {
+//     let result = 1;
+//     for (let i = 1; i <= n; i++) {
+//         result *= i;
+//     }
+//     return result;
+// }
+// let n = +prompt('Enter number', '');
+
+// if (n > 0 && Number.isInteger(n)) {
+//     alert(factorial(n));
+//     alert("Enter a number > 0");
+
+// } else {
+//     alert("It's wrong number");
+// }
+
+
+    // 5.
+// function number (num1, num2, num3) {
+//     return +(`${num1}${num2}${num3}`);
+// }
+// console.log(number(5, 8, 9));
+
+
+    // 6.
+    function calcSquare(x, y) {
+        
     }
-    return result;    
-}
-let n = +prompt('Enter number', '');
+    let x = +prompt('Enter  number', '');
 
-if (n > 0 && Number.isInteger(n)) {
-    alert(factorial(n));
-    alert("Enter a number > 0");
-
-} else {
-    alert("It's wrong number");
-}
+    // Norma 
+    // 1.
+    // 2.
+    
     
 
