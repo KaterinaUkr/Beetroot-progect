42e0e3cb156bedcd87d0cb4532dafd861a9b3be6
KaterinaUkr <sharavarova@gmail.com>
2023-04-28 02:43:06 +0300

    add homework 2

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-
+    // 1.Створити сторінку, що показує нумерований список пісень:
 let playList = [
 {
  author: "LED ZEPPELIN",
@@ -57,5 +56,26 @@ playList.forEach((item) => {
 console.log(createSongList(playList));
 
 
+// 2.Створити HTML-сторінку з кнопкою "Відкрити" і модальним вікном. На модальном вікні повинен бути текст і кнопка "Закрити". Спочатку модальне вікно не відображається. При кліку на кнопку "Відкрити" з'являється модальне вікно, на кнопку "Закрити" — зникає.
 
+let modal = document.querySelector('#modal');
+let open = document.querySelector('.btn-open');
+let close = document.querySelector('.btn-close');
+
+open.onclick = function () {
+    modal.style.display = 'block';
+}
+close.onclick = function () {
+    modal.style.display = 'none';
+}
+    //при натисканні за межами модального вікна, воно закривається
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+};
+
+
+
+// 3. Створити HTML-сторінку зі світлофором і кнопкою, яка перемикає світлофор на наступний колір.
 
