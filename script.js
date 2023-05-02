8c6c2e696a85adbae3079b55cab051253c1e0c44
KaterinaUkr <sharavarova@gmail.com>
2023-05-02 03:35:44 +0300

    add second task

--- script.js
+++ script.js
@@ -24,9 +24,49 @@ function editStart() {
     area.style.height = 150 + 'px';
     div.replaceWith(area);
     area.focus(); 
-}
+};
     
 function editEnd() {
     div.innerHTML = area.value;
     area.replaceWith(div);
-}
+};
+
+
+
+
+// Створити HTML-сторінку з великою таблицею. При кліку на заголовок стовпця, необхідно відсортувати дані цього стовпця. Врахуй, що числові значення повинні сортуватися як числа, а не як рядки.
+
+grid.onclick = function(e) {
+      if (e.target.tagName != 'TH') return;
+
+      let th = e.target;
+      // якщо клітинка TH, тоді сортувати
+      // cellIndex -- це номер клітинки th:
+      // 0 для першого стовпця
+      // 1 для другого і т.д.
+      sortGrid(th.cellIndex, th.dataset.type);
+    
+    };
+
+    function sortGrid(colNum, type) {
+      let tbody = grid.querySelector('tbody');
+      let rowsArray = Array.from(tbody.rows);
+      // compare(a, b) порівнює два рядки, необхідно для сортування
+      let compare;
+
+      switch (type) {
+          case 'number':              
+          compare = function(rowA, rowB) {
+            return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
+          };
+          break;
+        case 'string':
+        //   compare = function(rowA, rowB) {
+        //     return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1 : -1;
+        //   };
+          break;
+      }
+      // сортування
+      rowsArray.sort(compare);
+      tbody.append(...rowsArray);
+    }
