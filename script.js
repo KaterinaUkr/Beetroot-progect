4abef43cd1aed42a946fb70ddc89225c0aac207a
KaterinaUkr <sharavarova@gmail.com>
2023-05-03 10:28:56 +0300

    add slick

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.slick-carousel').slick({
+      infinite: true,
+      arrows: true,
+      
+    slidesToShow: 1,
+    slidesToScroll: 1,    
+    autoplay: true,
+    speed: 500,
+    dots: true,
+    variableWidth: true
+   
+  });
+});
