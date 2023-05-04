e4944a8b42ccee24454720f0b6f43c6a892dcb7e
KaterinaUkr <sharavarova@gmail.com>
2023-05-05 00:18:29 +0300

    add changes

---
+++ script.js
@@ -0,0 +1,66 @@
+// const promise = new Promise((resolve, reject) => {
+// 	resolve('resolve');
+// 	reject('reject')
+// });
+
+// promise.then((res) => console.log(res))
+
+
+// const promise = new Promise((resolve, reject) => {
+// 	try {
+// 		throw new Error()
+// 		resolve('resolve');
+// 	} catch (error) {
+// 		reject('reject fsxgftcdh')
+// 	}
+// });
+// promise.then((res) => console.log(res)).catch((e) => console.warn(e))
+
+// promise
+// 	.then{
+// 		(res) => console.log{ res }
+// 	return 'some'
+// }
+// 	.then((res2) => console.log(res2))
+// 	.catch((e) => console.warn(e))
+// 	.finally(() => console.log('finally'))
+
+
+// const API = 'https://jsonplaceholder.typicode.com/todos';
+
+// fetch(API, {
+// 	method: 'GET' | 'POST' | 'DELETE' | 'PUT',
+// 	body: 'some'
+// })
+// 	.then((res) => res.json())
+// 	.then((data) => console.log(data));
+
+
+
+// const todo = {
+// 		"id": 5,
+//     "title": "laboriosam",
+//     "completed": false
+// 	}
+	
+// 	fetch(API, {
+// 	method: 'POST',
+// 	body: JSON.stringify(todo)
+// })
+// 	.then((res) => res.json())
+// 	.then((data) => console.log(data))
+
+
+const API = 'https://swapi.dev/api/planets/';
+
+
+// fetch(API)
+// 	.then((res) => res.json())
+// 	.then((data) => {
+// 		const planets = data.results;
+// 		planets.map((item) => {
+// 			console.log(data.results)
+// 		})
+// 		);
+// 	}
+		
