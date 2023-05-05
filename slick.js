dda896cacf3a27213cbe5b6bba3794863ae47825
KaterinaUkr <sharavarova@gmail.com>
2023-05-06 00:59:49 +0300

    add hover

--- slick.js
+++ slick.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('.slider__image').slick({
-		autoplay: true,
+		// autoplay: true,
 		autoplaySpeed: 10000,
 		arrows: false,
 		dots: true,
@@ -10,13 +10,26 @@ $(document).ready(function(){
   });
 });
 
-
 $(document).ready(function(){
 	$('.product__slick').slick({
 		infinite: true,
-		slidesToShow: 4,
+		slidesToShow: 5,
 		slidesToScroll: 2,
 		arrows: true,
 		centerMode: true,		
+		centerPadding: 0,
+  });
+});
+
+$(document).ready(function(){
+	$('.partners__slick').slick({
+		infinite: true,
+		slidesToShow: 9,
+		slidesToScroll: 3,
+		arrows: true,
+		centerMode: true,		
+		adaptiveHeight: true,
+		centerPadding: 0,
+		
   });
 });
