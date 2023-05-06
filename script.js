fa3304f4fc1960016835184add21866124a6b07f
KaterinaUkr <sharavarova@gmail.com>
2023-05-06 09:25:15 +0300

    add changes

---
+++ script.js
@@ -0,0 +1,7 @@
+// const hamburgerElm = document.querySelector('.hamburger');
+//     const navElm = document.querySelector('.nav');
+
+
+//     hamburgerElm.addEventListener('click', () => {
+//         hamburgerElm.classList.toggle('is-active');
+//         navElm.classList.toggle('is-active');
--- slick.js
+++ slick.js
@@ -18,6 +18,41 @@ $(document).ready(function(){
 		arrows: true,
 		centerMode: true,		
 		centerPadding: 0,
+		responsive: [
+            {
+                breakpoint: 1300,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 2,
+                    // infinite: true,
+                    // dots: true
+                }
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+						},
+						{
+                breakpoint: 660,
+                settings: {
+                    slidesToShow: 1,
+										centerMode: true,
+										arrows: false,
+									dots: true,
+										
+                }
+            }
+        ]
   });
 });
 
