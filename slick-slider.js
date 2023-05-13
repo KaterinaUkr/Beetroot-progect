4b9c7f0bc88a597b07954995515f56391ce593f5
KaterinaUkr <sharavarova@gmail.com>
2023-05-14 01:47:47 +0300

    add changes

--- slick-slider.js
+++ slick-slider.js
@@ -2,7 +2,39 @@ $(document).ready(function(){
   $('.slick').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,   
+    dots: true,
+    // dots: boolean,
+    // centerPadding: '10px',
+    
+  //   responsive: [
+  //   {
+  //     breakpoint: 1024,
+  //     settings: {
+  //       slidesToShow: 3,
+  //       slidesToScroll: 3,
+  //       infinite: true,
+  //       dots: true
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 600,
+  //     settings: {
+  //       slidesToShow: 2,
+  //       slidesToScroll: 2
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       slidesToShow: 1,
+  //       slidesToScroll: 1
+  //     }
+  //   }
+  //   // You can unslick at a given breakpoint now by adding:
+  //   // settings: "unslick"
+  //   // instead of a settings object
+  // ]
     
   });
 });
