d20df7e16a5bb4d56d14ce2ab0431bd68e5fe41d
KaterinaUkr <sharavarova@gmail.com>
2023-05-14 08:11:06 +0300

    add all section

--- slick-slider.js
+++ slick-slider.js
@@ -2,8 +2,52 @@ $(document).ready(function(){
   $('.slick').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3,   
+    slidesToScroll: 2,   
     dots: true,
+    centerPadding: 0,    
+    
+  //   responsive: [
+  //   {
+  //     breakpoint: 1024,
+  //     settings: {
+  //       slidesToShow: 3,
+  //       slidesToScroll: 3,
+  //       infinite: true,
+  //       dots: true
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 600,
+  //     settings: {
+  //       slidesToShow: 2,
+  //       slidesToScroll: 2
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       slidesToShow: 1,
+  //       slidesToScroll: 1
+  //     }
+  //   }
+  //   // You can unslick at a given breakpoint now by adding:
+  //   // settings: "unslick"
+  //   // instead of a settings object
+  // ]
+    
+  });
+});
+		
+
+
+$(document).ready(function(){
+  $('.single-item').slick({
+    infinite: true,
+    arrows: Boolean,
+    // slidesToShow: 3,
+    // slidesToScroll: 3,   
+    dots: true,
+    // centerPadding: 0,
     // dots: boolean,
     // centerPadding: '10px',
     
@@ -38,4 +82,3 @@ $(document).ready(function(){
     
   });
 });
-		
