dd799a9b2d1a768b72766a0faa01000de1ebaac0
KaterinaUkr <sharavarova@gmail.com>
2023-05-18 13:52:03 +0300

    add minify-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
 
-function buildStyles() {
-  return 
-};
+// function buildStyles() {
+//   return 
+// };
+
+gulp.task('minify-css', () => {
+  return gulp.src('./style.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./'));
+});
 
 gulp.task('styles', function(){
     return gulp.src('./scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
+      .pipe(sass().on('error', sass.logError))
+      // .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('./'))
     .pipe(browserSync.stream());
 });
@@ -16,8 +24,9 @@ gulp.task('styles', function(){
 gulp.task('watch', function () {
   browserSync.init({
         server: "./"
-    });
-  gulp.watch('./scss/**/*', gulp.series(['styles']));
-  gulp.watch("./*.html", "./*.js").on('change', browserSync.reload);
-
+  });
+  
+  gulp.watch('./scss/**/*', gulp.series(['styles', 'minify-css']));
+  // gulp.watch("./*.html", "./*.js").on('change', browserSync.reload);
+  gulp.watch("./*.html").on('change', browserSync.reload);
 });
