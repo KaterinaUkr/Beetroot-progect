0d310283ea0b13e8ef792f5443834808c608550d
KaterinaUkr <sharavarova@gmail.com>
2023-05-20 00:27:08 +0300

    add first task

---
+++ script.js
@@ -0,0 +1,45 @@
+// // 1) Реалізуй клас, що описує коло.
+
+// class Circle {
+
+// 	constructor(radius) {
+// 		this.radius = radius;
+// 	}
+
+// 	get data() {
+// 		return `radius = ${this.radius}`;
+// 	}
+
+// 	get diam() {
+// 		return `diametr = ` + this.radius * 2 ;
+// 	}
+
+// 	set data(data) {
+// 		this.radius = data;
+// 	}
+
+// 	square() {
+// 		console.log(`the area of the circle is ` + (3.14 * (this.radius ** 2)));
+// 	}
+
+// 	long() {
+// 		console.log(`the circumference of the circle is ` + (3.14 * (this.radius*2)));
+// 	}
+// }
+
+// const number = new Circle(5);
+// console.log(number)
+// console.log(number.data);
+// console.log(number.diam);
+// number.square();
+// number.long();
+
+// number.data = 4;
+// console.log(number)
+// console.log(number.data);
+// console.log(number.diam);
+// number.square()
+// number.long()
+
+
+// 2)Реалізуй клас, що описує канцелярський маркер
